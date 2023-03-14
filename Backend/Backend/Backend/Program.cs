7eef011680ca187454f0854c83b2a5f43a08720f
LegendsZ <akkshai@yahoo.com>
2023-03-14 16:28:02 -0400

    backend initial commit

---
+++ Backend/Backend/Backend/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Jaabs Backend";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!\n");
+
+            Console.ReadKey(); //waits for keyinput
+        }
+    }
+}
