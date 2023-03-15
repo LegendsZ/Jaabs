1a70838b003a0772bfa20599f1c94f922dd8e4a3
LegendsZ <akkshai@yahoo.com>
2023-03-14 23:08:56 -0400

    Divided asd

--- Backend/Backend/Backend/Program.cs
+++ Backend/Backend/Backend/Program.cs
@@ -14,7 +14,7 @@ namespace Backend
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello World!\n");
 
-            Customer.Customer roland = new Customer.Customer("Roland", 69, "Somewhere in Toronto", new Customer.AccountDetails("3232", new Customer.Card("Chequing", 123, 123, 123, 10), null));
+            JAABS.Customer.Customer roland = new JAABS.Customer.Customer("Roland", 69, "Somewhere in Toronto", new JAABS.Customer.AccountDetails("3232", new JAABS.Customer.Card("Chequing", 123, 123, 123, 10), null));
             Console.WriteLine("Customer: {0}\nAddress: {1}\nAge: {2}\nCustomer Number: {3}\nTotal Cash: {4}", roland.Name, roland.Address, roland.Age, roland.Account.CustomerNumber, roland.Account.TotalCash());
 
             Console.ReadKey(); //waits for keyinput
