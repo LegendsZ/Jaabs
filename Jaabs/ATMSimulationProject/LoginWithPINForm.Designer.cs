dbad74970e6f564eef00bef5214dd46b829e79c7
jaideepkhaira <jaideep.khaira@ryerson.ca>
2023-03-31 12:50:53 -0400

    Removed Act number

--- Jaabs/ATMSimulationProject/LoginWithPINForm.Designer.cs
+++ Jaabs/ATMSimulationProject/LoginWithPINForm.Designer.cs
@@ -31,14 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginWithPINForm));
             this.btnLogin = new System.Windows.Forms.Button();
             this.txtboxPIN = new System.Windows.Forms.TextBox();
-            this.txtboxAccountNumber = new System.Windows.Forms.TextBox();
             this.lblPIN = new System.Windows.Forms.Label();
-            this.lblAccountNumber = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,9 +43,9 @@
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(207)))), ((int)(((byte)(240)))));
             this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnLogin.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.btnLogin.Location = new System.Drawing.Point(162, 246);
+            this.btnLogin.Location = new System.Drawing.Point(143, 208);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(125, 45);
+            this.btnLogin.Size = new System.Drawing.Size(150, 80);
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -59,7 +55,7 @@
             // 
             this.txtboxPIN.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.txtboxPIN.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtboxPIN.Location = new System.Drawing.Point(143, 195);
+            this.txtboxPIN.Location = new System.Drawing.Point(143, 156);
             this.txtboxPIN.MaxLength = 4;
             this.txtboxPIN.Name = "txtboxPIN";
             this.txtboxPIN.PasswordChar = '*';
@@ -67,68 +63,33 @@
             this.txtboxPIN.TabIndex = 4;
             this.txtboxPIN.UseSystemPasswordChar = true;
             // 
-            // txtboxAccountNumber
-            // 
-            this.txtboxAccountNumber.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtboxAccountNumber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtboxAccountNumber.Location = new System.Drawing.Point(129, 122);
-            this.txtboxAccountNumber.Margin = new System.Windows.Forms.Padding(0);
-            this.txtboxAccountNumber.MaxLength = 3;
-            this.txtboxAccountNumber.Name = "txtboxAccountNumber";
-            this.txtboxAccountNumber.Size = new System.Drawing.Size(200, 29);
-            this.txtboxAccountNumber.TabIndex = 3;
-            this.txtboxAccountNumber.TextChanged += new System.EventHandler(this.txtboxAccountNumber_TextChanged);
-            // 
             // lblPIN
             // 
             this.lblPIN.AutoSize = true;
             this.lblPIN.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblPIN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(207)))), ((int)(((byte)(240)))));
-            this.lblPIN.Location = new System.Drawing.Point(202, 167);
+            this.lblPIN.Location = new System.Drawing.Point(202, 128);
             this.lblPIN.Name = "lblPIN";
             this.lblPIN.Size = new System.Drawing.Size(45, 25);
             this.lblPIN.TabIndex = 1;
             this.lblPIN.Text = "PIN";
             this.lblPIN.Click += new System.EventHandler(this.lblPIN_Click);
             // 
-            // lblAccountNumber
-            // 
-            this.lblAccountNumber.AutoSize = true;
-            this.lblAccountNumber.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblAccountNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(207)))), ((int)(((byte)(240)))));
-            this.lblAccountNumber.Location = new System.Drawing.Point(143, 94);
-            this.lblAccountNumber.Name = "lblAccountNumber";
-            this.lblAccountNumber.Size = new System.Drawing.Size(165, 25);
-            this.lblAccountNumber.TabIndex = 0;
-            this.lblAccountNumber.Text = "Account Number";
-            this.lblAccountNumber.Click += new System.EventHandler(this.lblAccountNumber_Click);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::ATMSimulationProject.Properties.Resources.Tmu_black;
-            this.pictureBox1.Location = new System.Drawing.Point(162, 12);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(143, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 75);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(97, 122);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(29, 29);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.MidnightBlue;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(97, 195);
+            this.pictureBox3.Location = new System.Drawing.Point(97, 156);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(50, 29);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -142,20 +103,16 @@
             this.BackColor = System.Drawing.Color.MidnightBlue;
             this.ClientSize = new System.Drawing.Size(434, 321);
             this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtboxPIN);
-            this.Controls.Add(this.lblAccountNumber);
             this.Controls.Add(this.lblPIN);
-            this.Controls.Add(this.txtboxAccountNumber);
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.MaximizeBox = false;
             this.Name = "LoginWithPINForm";
             this.Text = "LoginWithPINForm";
             this.Load += new System.EventHandler(this.LoginWithPINForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -165,11 +122,8 @@
         #endregion
         private Button btnLogin;
         private TextBox txtboxPIN;
-        private TextBox txtboxAccountNumber;
         private Label lblPIN;
-        private Label lblAccountNumber;
         private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
         private PictureBox pictureBox3;
     }
 }
